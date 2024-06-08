6f9c4b9d2a873d1f51344b5ff02efef01ee9c43c
mortuusars <mortuusars@gmail.com>
2024-06-09 00:37:59 +0300

    Jade support for frames

--- common/src/main/java/io/github/mortuusars/exposure/integration/jade/ExposureJadePlugin.java
+++ common/src/main/java/io/github/mortuusars/exposure/integration/jade/ExposureJadePlugin.java
@@ -3,7 +3,9 @@ package io.github.mortuusars.exposure.integration.jade;
 import io.github.mortuusars.exposure.Exposure;
 import io.github.mortuusars.exposure.block.LightroomBlock;
 import io.github.mortuusars.exposure.block.entity.LightroomBlockEntity;
+import io.github.mortuusars.exposure.entity.PhotographFrameEntity;
 import io.github.mortuusars.exposure.integration.jade.component_provider.LightroomComponentProvider;
+import io.github.mortuusars.exposure.integration.jade.component_provider.PhotographFrameProvider;
 import net.minecraft.resources.ResourceLocation;
 import snownee.jade.api.IWailaClientRegistration;
 import snownee.jade.api.IWailaCommonRegistration;
@@ -13,6 +15,7 @@ import snownee.jade.api.WailaPlugin;
 @WailaPlugin(Exposure.ID)
 public class ExposureJadePlugin implements IWailaPlugin {
     public static final ResourceLocation LIGHTROOM = Exposure.resource("lightroom");
+    public static final ResourceLocation PHOTOGRAPH_FRAME = Exposure.resource("photograph_frame");
 
     @Override
     public void register(IWailaCommonRegistration registration) {
@@ -23,5 +26,6 @@ public class ExposureJadePlugin implements IWailaPlugin {
     @Override
     public void registerClient(IWailaClientRegistration registration) {
         registration.registerBlockComponent(LightroomComponentProvider.INSTANCE, LightroomBlock.class);
+        registration.registerEntityComponent(PhotographFrameProvider.INSTANCE, PhotographFrameEntity.class);
     }
 }
